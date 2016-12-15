b6cd090f98532577b7c8740b89e4ccb5e82f7355
Криворотова Ирина <kin2805@yandex.ru>
2016-12-16 02:09:55 +0600

    Установить Bootstrap 3

---
+++ www/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Kindergarten</title>
+
+    <link href="frontend/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="frontend/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+    <h1>Kindergarten</h1>
+</div>
+</body>
+</html>
