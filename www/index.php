40603ea5eddf09c979527bd3e94826da3a87062f
Криворотова Ирина <kin2805@yandex.ru>
2016-12-19 03:21:15 +0600

    Добавить форму на главной

--- www/index.php
+++ www/index.php
@@ -1,3 +1,5 @@
+<?php
+header('Content-Type: text/html; charset=utf-8]')?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -6,6 +8,7 @@
     <title>Kindergarten</title>
 
     <link href="frontend/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="frontend/css/style.css" rel="stylesheet">
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
@@ -13,7 +16,28 @@
 </head>
 <body>
 <div class="container">
-    <h1>Kindergarten</h1>
+    <div class="row top-place"></div>
+    <div class="row main-place">
+        <form id="main-form">
+            <div class="form-wrap">
+                <select id="user-type">
+                    <option>Детский сад</option>
+                    <option>Родитель</option>
+                </select>
+            </div>
+            <div class="form-wrap">
+                <input type="text" id="login" name="login" placeholder="Логин" />
+            </div>
+            <div class="form-wrap">
+                <input type="password" id="pass" name="pass" placeholder="Пароль" />
+            </div>
+            <div class="form-wrap">
+                <a href="#" id="registration">Регистрация</a>
+                <button type="submit" id="enter">Войти</button>
+            </div>   
+        </form>
+    </div>
+
 </div>
 </body>
 </html>
